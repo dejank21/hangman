fc36e1a52a01703269e685b1d9e36c7f54aef2bb
dejank21 <181667145+dejank21@users.noreply.github.com>
2025-05-14 13:07:03 -0500

    Fixed a bug in save_file.rb that caused the game to save the file in the wrong directory.

--- lib/save_file.rb
+++ lib/save_file.rb
@@ -10,6 +10,7 @@ class SaveFile
   def initialize(save_dir = nil, format = :json)
     @format = format
     @save_dir = save_dir || '.'
+    # Defaults to current directory if no save_dir is provided
     Dir.mkdir(@save_dir) unless Dir.exist?(@save_dir)
   end
 
@@ -34,7 +35,7 @@ class SaveFile
       serialized_data = SerializeDeserialize.serialize(game_data, @format)
 
       # Write the serialized data to a file
-      File.open(SAVE_FILENAME, 'w') do |file|
+      File.open(save_path, 'w') do |file|
         file.write(serialized_data)
       end
       # Used to handle [status, error] as provided when called in the game_logic class
@@ -49,7 +50,7 @@ class SaveFile
     return [:no_save, nil] unless save_exists?
 
     begin
-      serialized_data = File.read(SAVE_FILENAME)
+      serialized_data = File.read(save_path)
       # After reading the file, we deserialize the data
       game_data = SerializeDeserialize.deserialize(serialized_data, @format)
 
@@ -62,14 +63,14 @@ class SaveFile
 
   def save_exists?
     # Check if the save file exists
-    File.exist?(SAVE_FILENAME)
+    File.exist?(save_path)
   end
 
   def delete_save
     return [:no_save, nil] unless save_exists?
 
     begin
-      File.delete(SAVE_FILENAME)
+      File.delete(save_path)
       [:success, nil]
     rescue IOError => e
       [:error, e]
